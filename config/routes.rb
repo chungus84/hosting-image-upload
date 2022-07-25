d297018dbfe72cf5dcf48d26118b4e0c8b38a1e0
Kenichi Beveridge <kenbev84@gmail.com>
2022-07-25 20:50:36 +0100

    Added in gems and chnage article_path to root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'article#home'
+  root to: "articles#index"
   resources :articles, except: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
